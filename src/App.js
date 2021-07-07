2f9d69c6172ed2c86e0ed3ad0f319ef9653ce18e
Soumya Basnet <soumya.basnet.web@gmail.com>
2021-07-07 17:39:23 +0100

    added open source code link

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather Track
+      <h1>Weather Track</h1>
+      <footer>
+        This project is coded by {" "}
+        <a href=" https://soumyabasnet.com/ ">Soumya Basnet </a>{" "} ,
+          <a href="https://github.com/soumyabas/react-weather-track" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
-  );
+      );
 }
 
 
