0e5e455a5800817e48d1fe03b0fd00988d8d9806
Soumya Basnet <soumya.basnet.web@gmail.com>
2021-07-21 14:34:56 +0100

    new component for Day and date updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import Search from "./Search.js"
+import Search from "./Search.js";
+import DateToday from "./DateToday";
 
 export default function Weather(props) {
 
   const [weatherData, setWeatherData] = useState({ ready: false });
 
-  function handleResponse(response) {
-    console.log(response.data);
+  function handleResponse(response) {;
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
@@ -17,7 +17,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       iconImg: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-      date: "Wednesday 7:00"
+      date: new Date(response.data.dt * 1000),
     });
   }
 
@@ -28,7 +28,7 @@ export default function Weather(props) {
         <h2>{weatherData.city}</h2>
         <ul>
           <li>
-            {weatherData.date}
+            <DateToday date={weatherData.date} />
           </li>
           <li className="text-capitalize">
             {weatherData.description}
@@ -56,7 +56,7 @@ export default function Weather(props) {
     );
   } else {
     const apiKey = "e12d4984bdb9b1f3003f6782997bbdc8";
-   
+
     let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
     axios.get(apiUrl).then(handleResponse);
 
