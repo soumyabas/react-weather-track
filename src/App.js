baa793b6b1443dc7e71eb1820ee17bbd4758d8fd
Soumya Basnet <soumya.basnet.web@gmail.com>
2021-10-12 21:00:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ export default function App() {
         <Weather defaultCity="London" />
         <footer>
           This project is coded by {" "}
-          <a href=" https://soumyabasnet.com/ ">Soumya Basnet </a>, {" "} open-sourced on {" "}
-          <a href="https://github.com/soumyabas/react-weather-track" target=" ">GitHub</a>  , {" "} published on {" "}
-          <a href="https://elated-jepsen-72718b.netlify.app/" target=" ">netlify</a>
+          <a href=" https://soumyabasnet.com/ " target="_blank" rel="noopener noreferrer">Soumya Basnet </a>, {" "} open-sourced on {" "}
+          <a href="https://github.com/soumyabas/react-weather-track" target="_blank" rel="noopener noreferrer">GitHub</a>  , {" "} published on {" "}
+          <a href="https://elated-jepsen-72718b.netlify.app/" target="_blank" rel="noopener noreferrer">netlify</a>
         </footer>
       </div>
     </div>
